47f0e09d5c70c6057d1970243539c3ffdd928875
Chris Gold <chris.ratsimba87@gmail.com>
2018-02-12 14:47:51 +0100

    affichage +

--- php/query.php
+++ php/query.php
@@ -1,12 +1,12 @@
 <?php
 include 'link_db.php';
-$reqAff = "SELECT * FROM article ORDER BY date_crea DESC";
+$reqAff = "SELECT * FROM article ORDER BY date_modif DESC";
 $result = mysqli_query($cnx, $reqAff);
 
 if(mysqli_num_rows($result) > 0){
   while ($row = mysqli_fetch_assoc($result)){
     echo "<li>".$row['titre']."</li>";
-    echo "<li>".$row['auteur']."</li>";
+    echo "<li>".$row['auteur']." a creer cet article le ".$row['date_crea']."</li>";
     echo "<li>".$row['texte']."</li>";
     echo "<br>";
   };
