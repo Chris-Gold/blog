b87452594adfea92779d3581556b265f3fd679aa
magentall <magentallteller@gmail.com>
2018-02-12 17:24:42 +0000

    szh

--- index.php
+++ index.php
@@ -10,7 +10,11 @@
           <div class="row">
             <div id="idheadgch" class="col-sm-12 col-lg-4"></div>
             <div id="idheadmil" class="col-sm-12 col-lg-4 text-center animated flash">CSA BLOG</div>
-            <div id="idheaddrt" class="col-sm-12 col-lg-4"></div>
+            <div id="idheaddrt" class="col-sm-12 col-lg-4">
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Open modal for @mdo</button>
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@fat">Open modal for @fat</button>
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Open modal for @getbootstrap</button>
+            </div>
           </div>
         </div>
         <div class="container">
@@ -21,7 +25,8 @@
           </div>
         </div>
       </div>
-
+      <!-- modalz -->
+      <?php include 'includes/modalz.html'  ?>
 
       <!-- FOOTER -->
       <?php include 'includes/footer.html'  ?>
