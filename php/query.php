71b3fa059b7ffcf80a56d4857cbb16b310382053
Chris Gold <chris.ratsimba87@gmail.com>
2018-02-12 15:53:28 +0100

    heyhey

--- php/query.php
+++ php/query.php
@@ -1,13 +1,33 @@
 <?php
 include 'link_db.php';
-$reqAff = "SELECT * FROM article ORDER BY date_modif DESC";
-$result = mysqli_query($cnx, $reqAff);
+
+//Affichage des articles par date de modification
+$reqAffDown = "SELECT *, SUBSTR(texte 10) FROM article ORDER BY date_modif DESC LIMIT 5";
+$reqAffUp = "SELECT *, SUBSTR(texte, 1, 10) FROM article ORDER BY date_modif ASC LIMIT 5";
+
+//Affichage les articles de l'auteur...
+$reqFiltNom = "SELECT * FROM article WHERE article.auteur= ";
+//Affichage grouper par auteurs
+$reqFiltNomUp = "SELECT * FROM article GROUP BY auteur ASC";
+$reqFiltNomDown = "SELECT * FROM article GROUP BY auteur DESC";
+
+//Affichage des articles de la categorie...
+$reqFiltCat = "SELECT * FROM article WHERE article.categorie= ";
+//Affichage grouper par categorie
+$reqFiltCatUp = "SELECT * FROM article GROUP BY categorie ASC";
+$reqFiltCatDown = "SELECT * FROM article GROUP BY categorie DESC";
+
+
+
+
+$result = mysqli_query($cnx, $reqAffUp);
 
 if(mysqli_num_rows($result) > 0){
   while ($row = mysqli_fetch_assoc($result)){
-    echo "<li>".$row['titre']."</li>";
-    echo "<li>".$row['auteur']." a creer cet article le ".$row['date_crea']."</li>";
-    echo "<li>".$row['texte']."</li>";
+    echo "<li class=\"list-group-item disabled\">".$row['titre']."</li>";
+    echo "<li class=\"list-group-item\">Categorie: ".$row['categorie']."</li>";
+    echo "<li class=\"list-group-item \">".$row['auteur']." a creer cet article le ".$row['date_crea']."</li>";
+    echo "<li class=\"list-group-item \">".$row['texte']."</li>";
     echo "<br>";
   };
 }
