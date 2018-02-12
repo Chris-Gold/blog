b3f64d4d692335efc64eed798e0d6c01fcff5f3d
magentall <magentallteller@gmail.com>
2018-02-12 15:21:59 +0100

    sz

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
       <div id="id_site" class="main-container">
         <div id="idsite" class="container">
           <div class="row">
-            <div class="col-sm-12 col-lg-4"></div>
-            <div class="col-sm-12 col-lg-4"></div>
-            <div class="col-sm-12 col-lg-4"></div>
+            <div id="headgch" class="col-sm-12 col-lg-4"></div>
+            <div id="headmil" class="col-sm-12 col-lg-4 text-center"><h1>CSA BLOG</h1></div>
+            <div id="headdrt" class="col-sm-12 col-lg-4"></div>
           </div>
         </div>
         <div class="container">
           <div class="row">
-            <ul id="id_list_acc" /ul>
+            <ul id="id_list_acc" class="list-group" /ul>
           </div>
         </div>
       </div>
