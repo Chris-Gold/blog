f34689435579657f8208029b49a528ccd95aef40
magentall <magentallteller@gmail.com>
2018-02-12 12:17:23 +0100

    sz.01

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Blog</title>
+    <?php include 'includes/header.html'  ?>
+  </head>
+  <body id=idbody>
+    <div id=id_principal_cont class="container">
+      <div class="row">
+        <ul id="id_list_acc" /ul>
+      </div>
+    </div>
+    <!-- GESTION DU FOOTER -->
+    <?php include 'includes/footer.html'  ?>
+  </body>
+</html>
