f45a11d039bf88b07a05e66c4e596e64368fce6b
magentall <magentallteller@gmail.com>
2018-02-15 11:59:23 +0100

    sza15.2

--- php/link_db.php
+++ php/link_db.php
@@ -4,6 +4,9 @@ $usr= "csa";
 $password = "aform87";
 $dbName= "blog_af";
 
+$cnx=false;
+$cnxo=false;
+
 $cnx = mysqli_connect($serveur, $usr, $password, $dbName);
 
 $cnxo =  new mysqli($serveur, $usr, $password, $dbName);
